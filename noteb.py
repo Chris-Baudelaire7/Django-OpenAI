abc5df913c413795d458197ce1c9a8b8e4c26ab9
chris-baudelaire <chris.baudelaire77@gmail.com>
2023-03-29 02:41:44 +0200

    Initial Commit

---
+++ noteb.py
@@ -0,0 +1,17 @@
+# Your identification has been saved in /Users/new/.ssh/id_rsa
+# Your public key has been saved in /Users/new/.ssh/id_rsa.pub
+
+# The key fingerprint is:
+# SHA256:Qk8hxeFqDKF+/2v2ElHgaG6A5v513qIblR1B6uiqXxU new@Chris-Baudelaire.local
+# The key's randomart image is:
+# +---[RSA 3072]----+
+# |    . .o=+o      |
+# |   o . =.o..     |
+# |  + o + E..      |
+# | +   B *.+ .     |
+# |  o . O S..      |
+# | . . = +.        |
+# |  .   * ..       |
+# |   . + ==.       |
+# |  .o+ o=+=o      |
+# +----[SHA256]-----+
